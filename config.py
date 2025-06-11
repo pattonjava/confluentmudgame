28f33ae1667bdf6de7eb280bc8ff8144c8042679
pattonjava <105330336+pattonjava@users.noreply.github.com>
2025-06-10 23:45:01 -0400

    Adding config.py with updated player_events_topic
    feat: Add player_events_topic to config.py.  player_events is the Kafka topic name.

---
+++ config.py
@@ -0,0 +1,12 @@
+# config.py
+
+KAFKA_BOOTSTRAP_SERVERS = 'localhost:9092'
+PLAYER_COMMANDS_TOPIC = 'player_commands'
+GAME_EVENTS_TOPIC = 'game_events'
+PLAYER_EVENTS_TOPIC = 'player_events'
+# Add any other global configurations here
+
+MYSQL_HOST = 'localhost'
+MYSQL_USER = 'mysql_user_here'
+MYSQL_PASSWORD = 'mysql_pass_here'
+MYSQL_DATABASE = 'mysql_db_here'
